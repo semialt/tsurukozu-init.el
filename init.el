ea6c71fdfc1ff766c72c4c1d2cfee76ee817db27
semialt <semialt@users.noreply.github.com>
2014-08-05 00:15:04 +0900

    Create init.el

---
+++ init.el
@@ -0,0 +1,80 @@
+(create-fontset-from-ascii-font "Menlo-16:weight=normal:slant=normal" nil "tsurukozulogocon")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #x3000)
+       (decode-char 'ucs #x3002))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #x3008)
+       (decode-char 'ucs #x3011))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #x3013)
+       (decode-char 'ucs #x3019))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #x3031)
+       (decode-char 'ucs #x3032))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #x3037)
+       (decode-char 'ucs #x3037))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #x303C)
+       (decode-char 'ucs #x3094))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #x3099)
+       (decode-char 'ucs #x30FE))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF01)
+       (decode-char 'ucs #xFF01))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF03)
+       (decode-char 'ucs #xFF06))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF08)
+       (decode-char 'ucs #xFF1B))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF1D)
+       (decode-char 'ucs #xFF1D))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF1F)
+       (decode-char 'ucs #xFF3B))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF3D)
+       (decode-char 'ucs #xFF3D))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF3F)
+       (decode-char 'ucs #xFF3F))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF41)
+       (decode-char 'ucs #xFF5B))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFF5D)
+       (decode-char 'ucs #xFF65))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xFFE5)
+       (decode-char 'ucs #xFFE5))
+        "TBKanaBTsurukozu Std-18:weight=normal:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #x4e00)
+      (decode-char 'ucs #x9f8d))
+       "07LogoTypeGothic-Condense:slant=normal")
+  (set-fontset-font "fontset-tsurukozulogocon"
+    (cons (decode-char 'ucs #xffe3)
+      (decode-char 'ucs #xffe4))
+       "07LogoTypeGothic-Condense:slant=normal")
+ (add-to-list 'default-frame-alist '(font . "fontset-tsurukozulogocon"))
+
+
